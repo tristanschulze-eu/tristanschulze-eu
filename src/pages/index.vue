c7ef6cfb2672550db1b2121920320fea5e552610
Max Wolschlager <rxtx@ungut.at>
2024-09-21 17:18:11 +0200

    buggy image argh

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,13 +2,14 @@
   <div class="index w-100">
     <ContentDoc v-slot="{ doc }">
       <ContentRenderer class="px-4 py-6 w-100" :value="doc" />
-      
     </ContentDoc>
-    <nuxt-img class="index__image" src="/img/index/Tristan_Schulze-12.jpg" />
+    <div class="index__image h-100" >
+      <nuxt-img src="/img/index/Tristan_Schulze-12.jpg" />
+    </div>
   </div>
 </template>
   
-<style>
+<style lang="scss">
 .index {
   position: relative;
 }
@@ -17,7 +18,10 @@
   position: absolute;
   top: 0;
   left: 0;
-  width: 100%;
-  height: 100%;
+  overflow: hidden;
+  img {
+    min-height: 100%;
+    width: 100%;
+  }
 }
 </style>
